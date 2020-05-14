9303e83d0de4fe2e50ec1c4fc3b86944bc3d95ef
julie <julie@julies-MacBook-Pro.local>
2020-05-14 12:58:42 -0700

    pulled all workouts for user

--- src/components/createworkout/createworkout.js
+++ src/components/createworkout/createworkout.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react'
-import { createWorkout, userLogin } from '../../services/api-helper'
 import { WorkoutContext } from '../../App'
-import { Form } from 'react-bootstrap'
+import { createWorkout, deleteWorkout, getWorkouts } from '../../services/api-helper'
 
 export default function CreateWorkout(){
     const workoutContext = useContext(WorkoutContext)
@@ -11,17 +10,21 @@ export default function CreateWorkout(){
         workout_routine: '' 
       })
 
-      console.log("NEWOWRKOUT:", newWorkout)
-      console.log("CREATEWORK", workoutContext.user)
-
+      useEffect(() => {
+        const callAPI = async () => {
+            const res = await getWorkouts(workoutContext.user)
+            workoutContext.setWorkout(res.data)
+        }
+        callAPI()
+      }, [])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        await createWorkout(newWorkout, workoutContext.user)
+        const res = await createWorkout(newWorkout, workoutContext.user)
         .then(response => {
             if(response.token === 201){
-                console.log("RESPONSE!!!!!!!!!!")
-                workoutContext.workouts.push(newWorkout)
+                workoutContext.workout.push(res)
+                workoutContext.setWorkout(res) // works but needs to render on page after submit
             }
         })
     }
@@ -31,45 +34,71 @@ export default function CreateWorkout(){
         setNewWorkout({...newWorkout, [e.target.name]: value})
      }
 
+     // i don't work correctly
+     const handleDelete = async (response, index) => {
+        await deleteWorkout(workoutContext.user)
+        workoutContext.workout.splice(index, 1)
+        // workoutContext.setNewWorkout()
+        // workoutContext.setWorkout(workoutContext.workout)
+    }
+
+
     return (
-        <form onSubmit={ handleSubmit }>
+        <>
+            <form onSubmit={ handleSubmit }>
+                <select
+                    name="day" 
+                    value={newWorkout.day} 
+                    onChange={ handleChange } 
+                    placeholder="day" >
+                    <option>Sunday</option>
+                    <option>Monday</option>
+                    <option>Tuesday</option>
+                    <option>Wednesday</option>
+                    <option>Thursday</option>
+                    <option>Friday</option>
+                    <option>Saturday</option>
+                </select>
 
-            <select
-                name="day" 
-                value={newWorkout.day} 
-                onChange={ handleChange } 
-                placeholder="day" >
-                <option>Sunday</option>
-                <option>Monday</option>
-                <option>Tuesday</option>
-                <option>Wednesday</option>
-                <option>Thursday</option>
-                <option>Friday</option>
-                <option>Saturday</option>
-            </select>
+                <select
+                    name="workout" 
+                    value={newWorkout.workout} 
+                    onChange={ handleChange } 
+                    placeholder="day" >
+                    <option>shoulders</option>
+                    <option>arms</option>
+                    <option>back</option>
+                    <option>chest</option>
+                    <option>abs</option>
+                    <option>legs</option>
+                </select>
 
-            <select
-                name="workout" 
-                value={newWorkout.workout} 
-                onChange={ handleChange } 
-                placeholder="day" >
-                <option>shoulders</option>
-                <option>arms</option>
-                <option>back</option>
-                <option>chest</option>
-                <option>abs</option>
-                <option>legs</option>
-            </select>
+                <textarea 
+                    name="workout_routine" 
+                    value={newWorkout.workout_routine} 
+                    onChange={ handleChange } 
+                    placeholder='Workout Routine'
+                >
+                </textarea>
 
-            <textarea 
-                name="workout_routine" 
-                value={newWorkout.workout_routine} 
-                onChange={ handleChange } 
-                placeholder='Workout Routine'
-            >
-            </textarea>
+                <input type="Submit" value="Submit" />
+            </form>
 
-            <input type="Submit" value="Submit" />
-        </form>
+            <div>
+                {
+                workoutContext.workout ? 
+                    workoutContext.workout.map((workout, i) => { 
+                    return ( 
+                        <div key={i}>
+                            <h3>{workout.day}</h3>
+                            <p>{workout.workout}</p>
+                            <p>{workout.workout_routine}</p>
+                            <button onClick={handleDelete}>delete</button>
+                        </div>
+                        )
+                    }) : ''
+                }
+            </div>
+        </>
     )
 }
