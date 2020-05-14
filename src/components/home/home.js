571e96a5f373371d036bf09ddbcbac06c94d265e
julie <julie@julies-MacBook-Pro.local>
2020-05-14 12:59:34 -0700

    landing page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import { Card, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import './home.scss'
+import { CardBody } from 'reactstrap'
 
 export default function Home(){
     return (
-        <>This is the home page</>
+        <>
+            <Container>
+                <Card style={{ width: '12rem', height: '10rem' }}>
+                    <Card.Body>
+                        <Link to='/register'>Register</Link>
+                        <Link to='/register'><i className="fas fa-user-plus"></i></Link>
+                    </Card.Body>
+                </Card>
+
+                <Card style={{ width: '12rem', height: '10rem' }}>
+                    <Card.Body>
+                        <Link to='/profile'>Profile</Link>
+                    </Card.Body>    
+                </Card>
+
+                <Card style={{ width: '12rem', height: '10rem' }}>
+                    <Card.Body>
+                        <Link to='/exercise'>Excercise</Link>
+                        <Link to='/exercise'><i className="fas fa-bicycle"></i></Link>
+                    </Card.Body>
+                </Card>
+
+                <Card style={{ width: '12rem', height: '10rem' }}>
+                    <Card.Body>
+                        <Link to='/createworkout'>Create Workout</Link>
+                        <Link to='/createworkout'><i class="fas fa-dumbbell"></i></Link>
+                    </Card.Body>
+                </Card>
+            </Container>
+        </>
     )
 }
