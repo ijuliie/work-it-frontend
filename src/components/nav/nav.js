61a0cf4b94842e1ef0353cab5f32f34831bfe797
julie <julie@julies-MacBook-Pro.local>
2020-05-14 13:00:16 -0700

    add nav bar

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Navbar, Container, Navlink } from 'react-bootstrap'
+import { Navbar, Container } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
  
 export default function Nav() {
     return (
-        <Navbar>
-            <Container>
-                <Link to='login'>Login</Link>
-                <Link to='profile'>Profile</Link>
-                <Link to='exercise'>Exercise</Link>
-                <Link to='register'>Register</Link>
-            </Container>
-        </Navbar>
+        <>
+            <Navbar>
+                <Container>
+                    <Link to='login'>Login</Link>
+                    <Link to='profile'>Profile</Link>
+                    <Link to='createworkout'>Create Workout</Link>
+                    <Link to='register'>Register</Link>
+                </Container>
+            </Navbar>
+        </>
     )
 }
