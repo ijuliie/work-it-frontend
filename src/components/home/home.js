a28b1492960498bee2fe1f06ce29507d48b1ee85
julie <julie@julies-MacBook-Pro.local>
2020-05-15 11:00:22 -0700

    update

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -15,37 +15,37 @@ export default function Home(props){
 
     return (
             <Container>
-        { !workoutContext.loggedIn ?
-            <Card style={{ width: '13rem', height: '7.7rem' }}>
-                <Card.Body>
-                    <Link to='/register'>Register</Link>
-                    <div><Link to='/register'><i className="fas fa-user-plus"></i></Link></div>
-                </Card.Body>
-            </Card> :
+                { !workoutContext.loggedIn ?
+                    <Card style={{ width: '14rem', height: '7.7rem' }}>
+                        <Card.Body>
+                            <Link to='/register'>Register</Link>
+                            <div><Link to='/register'><i className="fas fa-user-plus"></i></Link></div>
+                        </Card.Body>
+                    </Card> :
 
-            <Card style={{ width: '13rem', height: '7.7rem' }}>
-            <Card.Body>
-                <div onClick={handleLogout}><Link to='#'>Log Out</Link></div>
-                <div><Link to='#'><i class="fas fa-sign-out-alt"></i></Link></div>
-            </Card.Body>
-            </Card> 
-        }
+                    <Card style={{ width: '14rem', height: '7.7rem' }}>
+                    <Card.Body>
+                        <div onClick={handleLogout}><Link to='#'>Log Out</Link></div>
+                        <div><Link to='#'><i class="fas fa-sign-out-alt"></i></Link></div>
+                    </Card.Body>
+                    </Card> 
+                }
 
-                <Card style={{ width: '13rem', height: '7.7rem' }}>
+                <Card style={{ width: '14rem', height: '7.7rem' }}>
                     <Card.Body>
                         <Link to='/profile'>Profile</Link>
                         <div><Link to='/profile'><i class="fas fa-user-circle"></i></Link></div>
                     </Card.Body>
                     </Card>
 
-                <Card style={{ width: '13rem', height: '7.7rem' }}>
+                <Card style={{ width: '14rem', height: '7.7rem' }}>
                     <Card.Body>
                         <Link to='/createworkout'>Create Workout</Link>
                         <div><Link to='/createworkout'><i className="fas fa-dumbbell"></i></Link></div>
                     </Card.Body>
                     </Card>
 
-                <Card style={{ width: '13rem', height: '7.7rem' }}>
+                <Card style={{ width: '14rem', height: '7.7rem' }}>
                     <Card.Body>
                         <Link to='/exercise'>Excercises</Link>
                         <div><Link to='/exercise'><i class="fas fa-running"></i></Link></div>
